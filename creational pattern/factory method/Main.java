46340ac8f93b14a941cb0773b6a850b115fd9b23
49EHyeon42 <49EHyeon42@gmail.com>
2022-05-17 16:04:57 +0900

    Add creational pattern/factory method

---
+++ creational pattern/factory method/Main.java
@@ -0,0 +1,27 @@
+// https://refactoring.guru/design-patterns/factory-method
+
+import factory.Dialog;
+import factory.HtmlDialog;
+import factory.WindowsDialog;
+
+public class Main {
+
+    private static Dialog dialog;
+
+    public static void main(String[] args) {
+        configure();
+        runBusinessLogic();
+    }
+
+    static void configure() {
+        if (System.getProperty("os.name").equals("Windows 10")) {
+            dialog = new WindowsDialog();
+        } else {
+            dialog = new HtmlDialog();
+        }
+    }
+
+    static void runBusinessLogic() {
+        dialog.renderWindow();
+    }
+}
