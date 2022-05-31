bc44bd660e3883b570e4948597d4a10462eead0b
49EHyeon42 <49EHyeon42@gmail.com>
2022-05-31 20:19:04 +0900

    Add behavioral pattern\memento

---
+++ behavioral pattern/memento/commands/ColorCommand.java
@@ -0,0 +1,28 @@
+package commands;
+
+import editor.Editor;
+import java.awt.Color;
+import shapes.Shape;
+
+public class ColorCommand implements Command {
+
+    private Editor editor;
+    private Color color;
+
+    public ColorCommand(Editor editor, Color color) {
+        this.editor = editor;
+        this.color = color;
+    }
+
+    @Override
+    public String getName() {
+        return "Colorize: " + color.toString();
+    }
+
+    @Override
+    public void execute() {
+        for (Shape child : editor.getShapes().getSelected()) {
+            child.setColor(color);
+        }
+    }
+}
